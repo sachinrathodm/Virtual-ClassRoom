c8fae4e8a10ff2722f4bac8d0deb993f303008ec
sachinrathodm <sachinrathodm@gmail.com>
2021-08-22 16:35:53 +0530

    front page

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const db = require('./jwt/config/database')
+const auth = require('./jwt/middleware/auth')
+
+db.connect()
+const app = express()
+app.use(express.json())
+
+//Route User
+app.get('/', (req, res) => {
+    res.send('Hello! Virtual Class Sachin!.....')
+})
+
+//Route User
+var user_router = require('./routes/userRoute')
+app.use('/user', user_router)
+
+//Route Assignment
+var assignment_router = require('./routes/assignmentSetRoute')
+app.use('/assignment', assignment_router)
+
+//Route Student Submission
+var submission_router = require('./routes/submissionRoute')
+app.use('/submission', submission_router)
+
+app.post('/welcome', auth, (req, res) => {
+    res.status(200).send('Welcome' + req.user.email)
+})
+
+module.exports = app
